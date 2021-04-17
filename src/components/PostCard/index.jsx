47d28e769326653ca021d19f5477817d1d7a2679
nico-rocha <nicoly.rochadm@gmail.com>
2021-04-17 16:54:18 -0300

    first commit

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export const PostCard = ({title, body, cover, id}) => (
+//const {post} = props
+//ou  direto {tile, cover, body, id}
+    <div className="post">
+        <img src={cover} alt={title}/>
+        <div className="post-content">
+            <h1>{title} {id}</h1>
+            <p>{body}</p>
+          </div>
+        </div>
+);
