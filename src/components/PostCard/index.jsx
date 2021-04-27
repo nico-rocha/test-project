2ae0b5ed62c89324128b91556ed133605741dbeb
nico-rocha <nicoly.rochadm@gmail.com>
2021-04-27 19:00:09 -0300

    class-to-hooks

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,13 +1,15 @@
-import './styles.css';
+import "./styles.css";
 
-export const PostCard = ({title, body, cover, id}) => (
-//const {post} = props
-//ou  direto {tile, cover, body, id}
-    <div className="post">
-        <img src={cover} alt={title}/>
-        <div className="post-content">
-            <h1>{title} {id}</h1>
-            <p>{body}</p>
-          </div>
-        </div>
+export const PostCard = ({ title, body, cover, id }) => (
+  //const {post} = props
+  //ou  direto {tile, cover, body, id}
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>
+        {title} {id}
+      </h1>
+      <p>{body}</p>
+    </div>
+  </div>
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,120 +1,80 @@
-import React from 'react';
+import { useEffect, useState, useCallback } from "react";
 
-import './styles.css';
+import "./styles.css";
 
-import { loadPosts } from '../../utils/load-posts';
-import { Posts } from '../../components/Posts';
-import { Button } from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
+import { loadPosts } from "../../utils/load-posts";
+import { Posts } from "../../components/Posts";
+import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
 
 
-export class Home extends React.Component{
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 2,
-    searchValue: ''
-};
 
- async componentDidMount() {
-    await this.loadPosts();
- };
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState("");
 
- loadPosts = async () => {
-    const{ page, postsPerPage} = this.state
+  const noMorePosts = page + postsPerPage >= allPosts.length;
 
+  const filteredPosts = !!searchValue
+  ? allPosts.filter(post => {
+      return post.title.toLowerCase().includes(
+        searchValue.toLowerCase()
+      );
+  })
+  : posts;
+
+  const handleLoadPosts = useCallback(async(page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos
-     });
- };
 
- loadMorePosts = () => {
-    const{
-      page, 
-      postsPerPage, 
-      allPosts,
-      posts
-    } = this.state;
+    setPosts(postsAndPhotos.slice(page, postsPerPage));  
+    setAllPosts(postsAndPhotos);
+  });
 
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+      
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
      posts.push(...nextPosts);
+      
+    setPosts(posts);
+    setPage(nextPage);
+  };
+      
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
 
-     this.setState({posts, page: nextPage});
-
- }
-
- handleChange = (e) =>{
-    const {value} = e.target;
-    this.setState({searchValue: value})
- }
-
-  render(){
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-
-    const filteredPosts = !!searchValue ? allPosts.filter(post =>{
-      return post.title.toLowerCase().includes(searchValue.toLowerCase()
-      );
-    })
-    : posts;
-
-     return (
+  return (
     <section className="container">
-
       <div className="search-container">
-      {!!searchValue && (
-         <h1>Search value: {searchValue}</h1>
-      )}
-     
-      <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        {!!searchValue && <h1>Search value: {searchValue}</h1>}
+
+        <TextInput
+          searchValue={searchValue}
+          handleChange={handleChange}
+        />
       </div>
 
-      {filteredPosts.length > 0 && (
-        <Posts posts={filteredPosts} />
-      )}  
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
 
-      {filteredPosts.length === 0 && (
-        <p>Não existem posts</p>
-      )}  
-      
+      {filteredPosts.length === 0 && <p>Não existem posts</p>}
 
       <div className="button-container">
         {!searchValue && (
           <Button
-            text="Load More Posts" 
-            onClick={this.loadMorePosts}
+            text="Load More Posts"
+            onClick={loadMorePosts}
             disabled={noMorePosts}
           />
         )}
-      
       </div>
     </section>
-   );
-  };
+  );
 };
-
-  // function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           olá
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
